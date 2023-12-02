80aef4168df39f4f8bf4fa4637aef3af23cf1b69
jbalaraman <james.balaraman@gmail.com>
2023-12-01 21:25:55 -0700

    used sorted() to compare strings

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, list):
+        new_list = []
+        for l in list:
+            if sorted(self.word) == sorted(l):
+                new_list.append(l)
+        return new_list
